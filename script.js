a49127d38f1483a064027e6388e5bee81b95086e
Damian <dsikora08@gmail.com>
2020-12-07 21:30:05 +0100

    Dodawanie klasy farmerOption--show po załadowaniu strony

--- script.js
+++ script.js
@@ -4,17 +4,30 @@
 
 window.onload = function startPage(){
 
-    let element1 = document.getElementsByClassName("element1");
-    element1[0].classList.add("element1--show");
+    // let element1 = document.getElementsByClassName("element1");
+    // element1[0].classList.add("element1--show");
 
-    let element2 = document.getElementsByClassName("element2");
-    element2[0].classList.add("element2--show");
+    // let element2 = document.getElementsByClassName("element2");
+    // element2[0].classList.add("element2--show");
 
-    let element3 = document.getElementsByClassName("element3");
-    element3[0].classList.add("element3--show");
+    // let element3 = document.getElementsByClassName("element3");
+    // element3[0].classList.add("element3--show");
 
-    let element4 = document.getElementsByClassName("element4");
-    element4[0].classList.add("element4--show");
+    // let element4 = document.getElementsByClassName("element4");
+    // element4[0].classList.add("element4--show");
 
+
+
+       let element1 = document.getElementsByClassName("element1");
+       element1[0].classList.add("farmerOption--show");
+
+       let element2 = document.getElementsByClassName("element2");
+       element2[0].classList.add("farmerOption--show");
+
+       let element3 = document.getElementsByClassName("element3");
+       element3[0].classList.add("farmerOption--show");
+
+       let element4 = document.getElementsByClassName("element4");
+       element4[0].classList.add("farmerOption--show");
 
 }
