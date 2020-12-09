85b5a7c431d1c4b86c25ddfc17b11e56cb864ddc
Damian <dsikora08@gmail.com>
2020-12-09 20:38:35 +0100

    Dodanie efektu rotacji elementu o 5 stopni po najechaniu kursorem myszy na element

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ window.onload = function startPage(){
         item.classList.add('farmerOption--show');
         })
 }
+
+let degreesChange=0;
+
+const addRotate5deg = function() {
+        degreesChange=5;
+        this.style.transform = `rotate(${degreesChange}deg)`;    
+}
+
+const deleteRotate5deg = function() {
+        this.style.transform = `rotate(${0}deg)`;
+}
+       
+const items = [...document.getElementsByClassName("farmerOption")];
+items.forEach((item) => item.addEventListener("mouseover", addRotate5deg))
+items.forEach((item) => item.addEventListener("mouseout", deleteRotate5deg))
+
+
+
+
