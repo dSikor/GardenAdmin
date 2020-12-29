5fe244ab3e9b7c0236aa01249340f5cd4534f4d0
Damian <dsikora08@gmail.com>
2020-12-29 20:33:24 +0100

    Stworzenie nowego wykresu kołowego

--- scriptsProducts.js
+++ scriptsProducts.js
@@ -7,19 +7,51 @@ var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+//     type: 'line',
 
-    // Configuration options go here
-    options: {}
+//     // The data for our dataset
+//     data: {
+//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//         datasets: [{
+//             label: 'My First dataset',
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [0, 10, 5, 2, 20, 30, 45]
+//         }]
+//     },
+
+  
+//     options: {}
+// });
+
+
+
+var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+                labels: ['Kury','Krowy','Zwierzeta domowe'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: ["#074D9", "#FF4136","rgb(23, 2, 132)"],
+                    hoverBorderColor: ["#074D2", "#FF4139","rgb(23, 2, 135)"],
+                    borderColor: 'rgb(55, 99, 132)',
+                    data: [40,55,5],
+                }]
+            },
+        
+            options: {
+                responsive: true,
+                title:{
+                    display: true,
+                    text: "Zwierzęta",
+                    fontColor: 'black'
+                },
+                legend: {
+                    labels: {
+                        fontColor: 'black'
+                    }
+                }
+            }
 });
 
 
