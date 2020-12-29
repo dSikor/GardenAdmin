3f7cf475106233572ac2a726a83603a4a1907991
Damian <dsikora08@gmail.com>
2020-12-29 19:27:59 +0100

    Zakomentowanie plinii kodu.

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ window.onload = function startPage(){
         })
 }
 
-let degreesChange=0;
+// let degreesChange=0;
 
-const addRotate5deg = function() {
-        degreesChange=5;
-        this.style.transform = `rotate(${degreesChange}deg)`;    
-}
+// const addRotate5deg = function() {
+//         degreesChange=5;
+//         this.style.transform = `rotate(${degreesChange}deg)`;    
+// }
 
-const deleteRotate5deg = function() {
-        this.style.transform = `rotate(${0}deg)`;
-}
+// const deleteRotate5deg = function() {
+//         this.style.transform = `rotate(${0}deg)`;
+// }
        
-const items = [...document.getElementsByClassName("farmerOption")];
-items.forEach((item) => item.addEventListener("mouseover", addRotate5deg))
-items.forEach((item) => item.addEventListener("mouseout", deleteRotate5deg))
+// const items = [...document.getElementsByClassName("farmerOption")];
+// items.forEach((item) => item.addEventListener("mouseover", addRotate5deg))
+// items.forEach((item) => item.addEventListener("mouseout", deleteRotate5deg))
 
 
 
