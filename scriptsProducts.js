2a455341facf025a90c2a3bb1283d4d9bb61e746
Damian <dsikora08@gmail.com>
2020-12-29 20:36:36 +0100

    Zakomentowanie wykresu

--- scriptsProducts.js
+++ scriptsProducts.js
@@ -3,9 +3,7 @@
 
 
 var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+// var chart = new Chart(ctx, {
 
 //     type: 'line',
 
